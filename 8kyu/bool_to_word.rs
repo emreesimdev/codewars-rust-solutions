e3fa7c6385613fdb123c662a7ef879cdd3cd6188
Emre Esim <emreesim.dev@gmail.com>
2026-01-11 16:07:38 +0300

    Solved: Bool to Word (8kyu)

---
+++ 8kyu/bool_to_word.rs
@@ -0,0 +1,12 @@
+/*
+  Title: Convert boolean values to strings 'Yes' or 'No'
+  Difficulty: 8 Kyu
+  Link: https://www.codewars.com/kata/53369039d7ab3ac506000467
+*/
+
+fn bool_to_word(value: bool) -> &'static str {
+    match value {
+        true => "Yes",
+        false => "No",
+    }
+}
