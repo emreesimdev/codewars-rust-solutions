44d9053f214a0a200da71fbc8f6fb085aedd9f7e
Emre Esim <emreesim.dev@gmail.com>
2026-01-11 21:03:43 +0300

    Solved: Keep Hydrated (8kyu)

---
+++ 8kyu/keep_hydrated.rs
@@ -0,0 +1,10 @@
+/*
+  Title: Keep Hydrated!
+  Difficulty: 8 Kyu
+  Link: https://www.codewars.com/kata/582cb0224e56e068d800003c
+*/
+
+fn litres(time: f64) -> i32 {
+    let litres = time * 0.5;
+    litres as i32 
+}
