2dfe4734f321df1a97b51fd3c105be71504a74ac
Emre Esim <emreesim.dev@gmail.com>
2026-01-11 21:24:33 +0300

    Solved: String repeat (8kyu)

---
+++ 8kyu/repeat_str.rs
@@ -0,0 +1,9 @@
+/*
+  Title: String repeat
+  Difficulty: 8 Kyu
+  Link: https://www.codewars.com/kata/57a0e5c372292dd76d000d7e
+*/
+
+fn repeat_str(src: &str, count: usize) -> String {
+    src.repeat(count)
+}
