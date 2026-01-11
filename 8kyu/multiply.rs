f427ff1fe0f0e56d6303ea7ae317282db1d5137c
Emre Esim <emreesim.dev@gmail.com>
2026-01-11 15:09:21 +0300

    Solved: Multiply (8kyu)

---
+++ 8kyu/multiply.rs
@@ -0,0 +1,12 @@
+/*
+  Title: Multiply
+  Difficulty: 8 Kyu
+  Link: https://www.codewars.com/kata/50654ddff44f800200000004
+
+  Description:
+  This code does not execute properly. Try to figure out why.
+*/
+
+fn multiply(a: i32, b: i32) -> i32 {
+    a * b
+}
