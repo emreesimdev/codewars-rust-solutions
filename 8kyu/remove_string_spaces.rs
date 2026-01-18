464b9011941a8fae0d8f9e430b4126b69723b78f
Emre Esim <emreesim.dev@gmail.com>
2026-01-18 13:27:57 +0300

    Solved: Remove String Spaces (8kyu)

---
+++ 8kyu/remove_string_spaces.rs
@@ -0,0 +1,9 @@
+/*
+  Title: Remove String Spaces
+  Difficulty: 8 Kyu
+  Link: https://www.codewars.com/kata/57eae20f5500ad98e50002c5
+*/
+
+fn no_space(x : String) -> String{
+  x.replace(" ","")
+}
